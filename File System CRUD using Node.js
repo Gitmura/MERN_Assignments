e776c30844a1829d2f052894009157bfc3b27a6e
Amura_RNSIT <amura@rnsit.ac.in>
2025-04-15 15:41:56 +0530

    Create File System CRUD using Node.js

---
+++ File System CRUD using Node.js
@@ -0,0 +1,25 @@
+Title: File System Operations
+
+Tasks:
+
+Implement Create, Read, Update, and Delete on files using Node.js.
+
+Skills: Node.js fs module, sync & async methods
+
+# Steps---
+
+1. Initialize Node.js project:
+  mkdir file-crud && cd file-crud
+  npm init -y
+
+2. Use fs module in index.js.
+
+3. Implement:
+
+  Create (writeFile)
+
+  Read (readFile)
+
+  Update (appendFile or overwrite)
+
+  Delete (unlink)
